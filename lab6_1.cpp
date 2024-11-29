21638c3051dad47645f3e1df5b4a501c94b196be
HoriKana <arialfuaral36118@gmail.com>
2024-11-29 16:10:37 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int x;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    int odd = 0;
+    int even = 0;
+    while(x!=0){
+        if(x%2==0){
+            even+=1;
+        }
+        else{
+            odd+=1;
+        }
+    cout << "Enter an integer: ";
+    cin>>x;
+    }
+
+    cout << "#Even numbers = "<<even<<"\n";
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
